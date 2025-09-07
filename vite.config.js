68a7279f13f7028e56e23b63f9b15e53ad31b1ec
Remi Stocker <remigio18.96@gmail.com>
2025-09-07 13:48:11 -0300

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PizzeriaMM_2_Estados_y_componentes/',
+  base: '/PizzeriaMM_2_Estados_y_componentes/', // 👈 importante
 })
