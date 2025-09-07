528c5a63a543bc00ae6e41276e1555a601a93bad
Remi Stocker <167483558+Remigio96@users.noreply.github.com>
2025-09-07 13:31:12 -0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PizzeriaMM/',
+- base: '/PizzeriaMM/',
++ base: '/PizzeriaMM_2_Estados_y_componentes/',
 })
